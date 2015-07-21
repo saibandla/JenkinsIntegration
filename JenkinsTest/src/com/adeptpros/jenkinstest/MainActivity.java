2a8befe3e7df59e6fc2773269e5b7afb3386b828
Adeptpros <bbandla@genyedu.com>
2015-07-21 12:12:06 +0530

    Added Button

--- JenkinsTest/src/com/adeptpros/jenkinstest/MainActivity.java
+++ JenkinsTest/src/com/adeptpros/jenkinstest/MainActivity.java
@@ -4,14 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+	Button buttion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttion=(Button)findViewById(R.id.button1);
+        buttion.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
     }
 
     @Override
